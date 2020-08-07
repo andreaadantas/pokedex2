07d5ec1247a616eff495e29d1ad7e1e02898144d
Andrea Dantas <andreadantas695@gmail.com>
2020-08-07 14:52:14 -0400

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by andrea dantas on 07/08/20.
+//  Copyright © 2020 apple developer academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
